1ef2c5f438449469cf81e8f0a5aa765984c13398
gusailov <gusailov@gmail.com>
2020-11-13 12:18:38 +0200

    3

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,36 +2,32 @@ import React, { useState } from "react";
 import Item from "./Item";
 import Filter from "./Filter";
 
-const ListItems =(props)=> {
-  
+const ListItems = (props) => {
   const [searchTerm, setsearchTerm] = useState("");
-  
+  console.log(props);
   const updateFilter = ({ target }) => {
     setsearchTerm(target.value);
   };
-  
- const getBody = () => {
-      const { title, items } = props;
+
+  const getBody = () => {
+    const { title, items } = props;
     let out = [...items];
-     if (searchTerm) {
-      out = out.filter(item =>
+    if (searchTerm) {
+      out = out.filter((item) =>
         item.value.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
-        return out.map(item => <Item title={title} item={item} key={item.id} />);
-  }
-  
-   
-    return (
-     
-      <section>
-        <h3 className="mb-3">Title</h3>
-        <Filter filter={searchTerm} onChange={updateFilter} />
+    return out.map((item) => <Item title={title} item={item} key={item.id} />);
+  };
 
-        <ul className="mb-3 p-0">{getBody()}</ul>
-      </section>
-    );
-  
-}
+  return (
+    <section>
+      <h3 className="mb-3">Title</h3>
+      <Filter filter={searchTerm} onChange={updateFilter} />
+
+      <ul className="mb-3 p-0">{getBody()}</ul>
+    </section>
+  );
+};
 
 export default ListItems;
--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,40 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { generate as id } from "shortid";
 
-class NewItem extends Component {
-  state = {
-    value: ""
-  };
+const NewItem = (props) => {
+  const [value, setValue] = useState("");
+  // state = {
+  //   value: "",
+  // };
 
-  handleChange = ({ target }) => this.setState({ value: target.value });
+  const handleChange = ({ target }) => setValue(target.value);
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addItem(this.state.value);
-    this.setState({ value: "" });
+    props.addItem(value);
+    setValue(" ");
   };
 
-  render() {
-    const { value } = this.state;
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="row">
-          <div className="col-md-10">
-            <input
-              className="form-control mb-3"
-              type="text"
-              onChange={this.handleChange}
-              value={value}
-            />
-          </div>
-          <div className="col-md-2">
-            <input className="btn btn-success" type="submit" value="Add item" />
-          </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-md-10">
+          <input
+            className="form-control mb-3"
+            type="text"
+            onChange={handleChange}
+            value={value}
+          />
         </div>
-      </form>
-    );
-  }
-}
+        <div className="col-md-2">
+          <input className="btn btn-success" type="submit" value="Add item" />
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default NewItem;
